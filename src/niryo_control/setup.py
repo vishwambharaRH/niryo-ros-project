fc1d017e27e6ec492c10dfd5f3e381138c8fa16b
Vishwambhara R Hebbalalu <vishwambharah@gmail.com>
2025-11-18 08:44:29 +0530

    saved files

--- src/niryo_control/setup.py
+++ src/niryo_control/setup.py
@@ -1,28 +1,30 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'niryo_control'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/config', ['config/camera_params.yaml'] if package_name=='gesture_recognition' else []),
-        ('share/' + package_name + '/config', ['config/gesture_params.yaml'] if package_name=='gesture_recognition' else []),
-        ('share/' + package_name + '/config', ['config/gesture_mappings.yaml'] if package_name=='gesture_control_interface' else []),
-        ('share/' + package_name + '/launch', []),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
     maintainer_email='you@example.com',
-    description='niryo_control',
+    description='Robot controller for Niryo Ned2 (real and simulation)',
     license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "robot_controller = niryo_control.robot_controller:RobotController"
+            'sim_robot_controller = niryo_control.sim_robot_controller:main',
+            'real_robot_controller = niryo_control.real_robot_controller:main',
         ],
     },
 )
